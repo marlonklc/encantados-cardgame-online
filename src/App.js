974a77576edbef8f4ddeb33e0cd84536e35b081f
Marlon Klagenberg <marlon.conceicao@hypeflame.com.br>
2023-03-02 17:49:40 -0300

    improved configs to game gonna be runned on cloud/external

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Client } from 'boardgame.io/react';
 import { Board } from './Board';
 import { EncantadosGame } from './Game';
 import { SocketIO } from 'boardgame.io/multiplayer';
+import { DEFAULT_PORT, APP_PRODUCTION } from './config';
+
+const { origin, protocol, hostname } = window.location;
+const SERVER_URL = APP_PRODUCTION ? origin : `${protocol}//${hostname}:${DEFAULT_PORT}`;
 
 const EncantadosGameClient = Client({
     game: EncantadosGame,
     board: Board,
-    // debug: false,
-    multiplayer: SocketIO({ server: 'localhost:8000' }),
+    debug: false,
+    multiplayer: SocketIO({ server: SERVER_URL }),
     numPlayers: 2
 });
 
@@ -16,18 +20,17 @@ export default class App extends React.Component {
     state = { playerID: null, matchID: null };
 
     componentDidMount() {
-        const matchID = localStorage.getItem('matchID') || new Date().toISOString();
+        const matchID = new URLSearchParams(window.location.search).get('matchID');
 
-        localStorage.setItem('matchID', matchID);
+        if (matchID === null) {
+            window.location.search = `matchID=match-${new Date().getTime()}`
+        }
 
         this.setState({ matchID })
     }
 
     resetGame = () => {
-        const newMatchID = new Date().toISOString();
-
-        localStorage.setItem('matchID', newMatchID);
-        this.setState({ matchID: newMatchID });
+        window.location.search = `matchID=match-${new Date().getTime()}`
 
         alert('Jogo foi resetado!');
     }

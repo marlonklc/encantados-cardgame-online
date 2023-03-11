c06fa8dc88d3ef8d79bdc445fe01f7086f3041fb
Marlon Klagenberg <marlon.conceicao@hypeflame.com.br>
2023-03-10 23:06:48 -0300

    splitting responsability between server and client react.

--- src/server.js
+++ src/server.js
@@ -1,28 +1,12 @@
 const { Server, Origins } = require('boardgame.io/server');
 const { EncantadosGame } = require('./Game');
 const { DEFAULT_PORT } = require('./config');
-const path = require('path');
-const serve = require('koa-static');
 
-const PORT = process.env.PORT_SRC_SERVER || process.env.PORT_ENCANTADOSONLINE_SRC_SERVER || DEFAULT_PORT;
+const PORT = process.env.PORT_ENCANTADOSONLINE_SRC_SERVER || DEFAULT_PORT;
 
 const server = Server({
     games: [EncantadosGame],
     origins: ['http://www.marlonklc.kinghost.net', Origins.LOCALHOST]
 });
 
-const frontEndAppBuildPath = path.resolve(__dirname, "../build");
-server.app.use(serve(frontEndAppBuildPath));
-
-server.run({
-    port: PORT,
-    callback: () => {
-        async (ctx, next) => await serve(frontEndAppBuildPath)(
-            Object.assign(ctx, { path: 'index.html' }),
-            next
-        )
-    },
-    // lobbyConfig: {
-    //   uuid: customAlphabet("ABCDEFGHJKMNOPQRSTUVWXYZ0123456789", 6),
-    // },
-});
+server.run({ port: PORT });

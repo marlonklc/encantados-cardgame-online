4d8e3de04e97b8e8d6b31bdc2fd4cc4d971cd353
Marlon Klagenberg <marlon.conceicao@agi.com.br>
2023-11-04 20:19:15 -0300

    adjust on initial screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsEmojiSmile } from 'react-icons/bs';
+import { LiaUserCircle } from 'react-icons/lia';
 import { Client } from 'boardgame.io/react';
 import { Board } from './Board';
 import { EncantadosGame } from './Game';
@@ -39,24 +40,31 @@ export default class App extends React.Component {
                         <button 
                             onClick={() => this.setState({ playerID: "0" })}
                             disabled={false}
+                            class="button-players player1"
                         >
+                            <i><LiaUserCircle/></i>
                             Player 1
                         </button>
                         <button 
                             onClick={() => this.setState({ playerID: "1" })}
                             disabled={false}
+                            class="button-players player2"
                         >
+                            <i><LiaUserCircle/></i>
                             Player 2
                         </button>
                     </section>
-                    <br />
-                    <p><b>Como jogar ?</b></p>
-                    <p>1. Escolha com qual player quer jogar.</p> 
-                    <p>2. Envie o link pro seu amigo e peça para ele escolher o outro jogador <BsEmojiSmile /></p>
-                    <br />
-                    <h3>Links</h3>
-                    <a href="https://ludopedia.com.br/jogo/encantados-3-edicao" target="_blank" rel="noopener noreferrer">mais detalhes sobre o jogo</a>
-                    <a href="https://ludopedia.com.br/download.php?id_jogo_anexo=179044" target="_blank" rel="noopener noreferrer">manual do jogo</a>
+                    <section class="initial-screen-description">
+                        <p><b>Como jogar ?</b></p>
+                        <p>1. Escolha com qual player quer jogar.</p> 
+                        <p>2. Envie o link pro seu amigo e peça para ele escolher o outro jogador</p>
+                        <p>3. Divirta-se <BsEmojiSmile/></p>
+                        <br />
+                        <h3>Links</h3>
+                        <a href="https://ludopedia.com.br/jogo/encantados-3-edicao" target="_blank" rel="noopener noreferrer">mais detalhes sobre o jogo</a>
+                        <br/>
+                        <a href="https://ludopedia.com.br/download.php?id_jogo_anexo=179044" target="_blank" rel="noopener noreferrer">manual do jogo</a>
+                    </section>
                 </div>
             );
         }

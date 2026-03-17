21749a8921dba9a954e3eb829c2a86c24b8d2271
Marlon Klagenberg <marlon.klc@gmail.com>
2026-03-16 21:29:32 -0300

    Refactor server origins to use environment variable

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ const { EncantadosGame } = require('./Game');
 const { DEFAULT_PORT } = require('./config');
 
 const PORT = process.env.PORT_ENCANTADOSONLINE_SRC_SERVER || DEFAULT_PORT;
+const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN || 'http://localhost:8000';
 
 const server = Server({
     games: [EncantadosGame],
-    origins: ['http://www.marlonklc.kinghost.net', Origins.LOCALHOST]
+    origins: [
+        ALLOW_ORIGIN, 
+        Origins.LOCALHOST
+    ]
 });
 
 server.run({ port: PORT });
